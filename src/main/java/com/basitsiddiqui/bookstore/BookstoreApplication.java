409639a7a3f511df3bfd607a79cd2d98f5b9b7b8
Basit Siddiqui <hello@basitsiddiqui.com>
2020-09-16 14:11:12 +0100

    "cleanup to initial state - removed microservices"

--- src/main/java/com/basitsiddiqui/bookstore/BookstoreApplication.java
+++ src/main/java/com/basitsiddiqui/bookstore/BookstoreApplication.java
@@ -7,22 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
-
-	@GetMapping("/")
-	public String welcome(){
-		return "<H1>Welcome Stranger!</H1>";
-	}
-
-	@GetMapping("/{name}")
-	public String welcome(@PathVariable String name){
-		return "<H1>Welcome " + name + "!</H1>";
-	}
-
-
 }
