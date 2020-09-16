1e05a0d1a0e587930e7b12c1ee7dfc9b70e26d8c
Basit Siddiqui <hello@basitsiddiqui.com>
2020-09-16 13:32:51 +0100

    "with inline microservice in main Springboot application"

--- src/main/java/com/basitsiddiqui/bookstore/BookstoreApplication.java
+++ src/main/java/com/basitsiddiqui/bookstore/BookstoreApplication.java
@@ -2,12 +2,20 @@ package com.basitsiddiqui.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String welcome(){
+		return "<H1>Welcome!</H1>";
 	}
 
 }
